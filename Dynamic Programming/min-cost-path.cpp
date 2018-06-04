7ec8c96407f5d05fcda292bf44e1b4e6f16331e2
abhisheksingla1000 <singla.abhishek97@gmail.com>
2018-06-04 20:56:52 +0530

    Update min-cost-path.cpp

--- Dynamic Programming/min-cost-path.cpp
+++ Dynamic Programming/min-cost-path.cpp
@@ -10,7 +10,6 @@ using namespace std;
 int cost[10][10];
 int mincost[10][10];
 int getmincost(int m, int n){
-	cout<<mincost[m][n]<<"  "<<cost[m][n]<<endl;
 	if (mincost[m][n] == -1){
 		mincost[m][n] = min(getmincost(m-1,n-1),min(getmincost(m-1,n),getmincost(m,n-1)))+cost[m][n];
 	}
@@ -35,12 +34,6 @@ int main(){
 	}
 	for (int j = 1; j < c; ++j){
 		mincost[0][j] = mincost[0][j-1] + cost[0][j];
-	}
-	for (int i = 0; i < r; ++i){
-		cout<<endl;
-		for (int j = 0; j < c; ++j){
-			cout<<mincost[i][j]<<"\t";
-		}
 	}
 	cout<<endl;
 	cin>>m>>n;
